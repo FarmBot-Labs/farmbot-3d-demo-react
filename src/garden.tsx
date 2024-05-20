9e23c50e5b8eba29fbeddb97008a284e1b4234fa
Rory Aronson <rory@farmbot.io>
2024-05-20 15:07:30 -0700

    add blue sky

--- src/garden.tsx
+++ src/garden.tsx
@@ -4,9 +4,9 @@ import {
   GizmoHelper, GizmoViewcube,
   OrbitControls, PerspectiveCamera,
   Circle, Stats, Billboard, Text, Image, Clouds, Cloud, OrthographicCamera,
-  Detailed,
+  Detailed, Sphere,
 } from "@react-three/drei";
-import { TextureLoader, RepeatWrapping, Vector3 } from "three";
+import { TextureLoader, RepeatWrapping, Vector3, BackSide } from "three";
 import { Bot } from "./bot";
 import { Bed } from "./bed";
 import { threeSpace, zZero } from "./helpers";
@@ -58,10 +58,6 @@ interface Plant {
 const Model = (props: ModelProps) => {
   const { config } = props;
   const groundZ = config.bedZOffset + config.bedHeight;
-  const midPoint = {
-    x: threeSpace(config.bedLengthOuter / 2, config.bedLengthOuter),
-    y: threeSpace(config.bedWidthOuter / 2, config.bedWidthOuter),
-  };
   const Camera = config.perspective ? PerspectiveCamera : OrthographicCamera;
 
   const gardenPlants = GARDENS[config.plants] || [];
@@ -168,8 +164,8 @@ const Model = (props: ModelProps) => {
     <Circle name={"ground"}
       visible={config.ground}
       receiveShadow={true}
-      args={[30000, 100]}
-      position={[midPoint.x, midPoint.y, -groundZ]}>
+      args={[30000, 16]}
+      position={[0, 0, -groundZ]}>
       {children}
     </Circle>;
 
@@ -193,6 +189,9 @@ const Model = (props: ModelProps) => {
       mieDirectionalG={0.9}
       rayleigh={3}
       turbidity={5} />
+    <Sphere args={[30000, 8, 16]}>
+      <meshBasicMaterial color={"#59d8ff"} side={BackSide} />
+    </Sphere>
     <animated.group scale={props.activeFocus ? 1 : scale}>
       <Camera makeDefault={true} name={"camera"}
         fov={40} near={10} far={75000}
@@ -263,7 +262,7 @@ const Model = (props: ModelProps) => {
       outlineBlur={20}
       outlineOpacity={0.75}
       position={[
-        midPoint.x,
+        0,
         threeSpace(-500, config.bedWidthOuter),
         -groundZ + 100,
       ]}
